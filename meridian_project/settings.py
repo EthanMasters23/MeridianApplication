42b874c96fc8c250dc5685b5707700ad3442fd73
Ethan Masters <ethansmasters@outlook.com>
2023-06-18 09:11:35 +0200

    initial website commit

--- meridian_project/settings.py
+++ meridian_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.meridian-website.herokuapp.com"]
 
 # Added
 LOGIN_URL = 'login'
